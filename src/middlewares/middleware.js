e6bc0b494299c4c6f53a040553b1c4f798039613
Diogo <alves.diogoj@gmail.com>
2023-08-08 00:03:40 -0300

    Cadastrando contatos (Create)

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -18,7 +18,16 @@ exports.checkCsrfError = (err, req, res, next) => {
     next();
   };
   
-  exports.csrfMiddleware = (req, res, next) => {
-    res.locals.csrfToken = req.csrfToken();
-    next();
-  };
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+};
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Voê precisa fazer login');
+    req.session.save(() => res.redirect('/') );
+    return;
+  }
+  next();
+};
